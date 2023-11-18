8233c5554bcede28951ae29ab54e850a5f0b73dc
ryu <jeungwon28@gmail.com>
2023-11-19 00:20:56 +0900

    ADD: 가변인자

---
+++ src/Grammar/VarArgsTest.java
@@ -0,0 +1,26 @@
+package Grammar;
+
+public class VarArgsTest {
+    public static void main(String[] args) {
+        String[] strArr = { "100", "200", "300" };
+
+        System.out.println(concatenate("", "100", "200", "300"));
+        System.out.println(concatenate("-", strArr));
+        System.out.println(concatenate(",", new String[]{"1", "2", "3"}));
+        System.out.println("["+concatenate(",", new String[0])+"]");
+        System.out.println("["+concatenate(",")+"]");
+    }
+
+    /**
+     * 가변인자는 마지막 매개변수여야 함
+     */
+    static String concatenate(String delim, String... args) {
+        String result = "";
+
+        for(String str : args) {
+            result += str + delim;
+        }
+
+        return result;
+    }
+}
