cb6d223da2294b9fb101092c92d41f6e6cd895b9
ryu <jeungwon28@gmail.com>
2023-12-15 23:46:15 +0900

    ADD: NullPointExption과 Null 관련 주의점과 생각해야할 점

---
+++ inflearn/basic/ref/NullMain3.java
@@ -0,0 +1,13 @@
+package ref;
+
+public class NullMain3 {
+
+    public static void main(String[] args) {
+        BigData bigData = new BigData();
+        System.out.println("bigData.count=" + bigData.count);
+        System.out.println("bigData.data=" + bigData.data); //null로 초기화됌
+
+        //NullPointerException
+        System.out.println("bigData.data.value" + bigData.data.value);
+    }
+}
