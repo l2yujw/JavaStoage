31951afde7fc1fd4bc1df6a6df78fc63a5b9ed92
ryu <jeungwon28@gmail.com>
2023-12-16 17:18:15 +0900

    ADD: package 관리 및 규칙

---
+++ inflearn/basic/pack/PackageMain1.java
@@ -0,0 +1,12 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain1 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+//        pack.a.user user = new pack.a.User();
+        User user = new User();
+    }
+}
---
+++ inflearn/basic/pack/a/User.java
@@ -0,0 +1,8 @@
+package pack.a;
+
+public class User {
+
+    public User() {
+        System.out.println("패키지 Pack.a 회원 생성");
+    }
+}
